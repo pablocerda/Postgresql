f9136c0a4b2f4a7b31ab101a67f5741390bd8350
pablo <you@example.com>
2022-07-18 15:44:17 -0400

    Conexion finalizada

---
+++ conexion.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_1'
+)
+
+# print(conexion)
+
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia)
+registros = cursor.fetchall()
+print(registros)
+
+cursor.close()
+conexion.close()
